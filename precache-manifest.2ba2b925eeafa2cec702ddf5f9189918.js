ce8c842fcee8be1450104faf2068f96f68c39564
Anushree <anushreepm13@gmail.com>
2020-07-21 22:12:33 +0530

    Updates

---
+++ precache-manifest.2ba2b925eeafa2cec702ddf5f9189918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d979095f385ddecf7158e0179f315ab5",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "5420941e008799650fc3",
+    "url": "/Notes-App/static/css/main.70aea3ad.chunk.css"
+  },
+  {
+    "revision": "c11b1bbe19e81e236931",
+    "url": "/Notes-App/static/js/2.76605d2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Notes-App/static/js/2.76605d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5420941e008799650fc3",
+    "url": "/Notes-App/static/js/main.c1eed7b1.chunk.js"
+  },
+  {
+    "revision": "5f3befb0559d90f61ad5",
+    "url": "/Notes-App/static/js/runtime-main.b3d9d431.js"
+  }
+]);
